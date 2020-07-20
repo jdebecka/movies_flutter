1fec7c9032ac72434c49eb128ecd20c1c291dc92
Adrian Stasiak <adrian.stasiak@accenture.com>
2020-07-20 12:57:02 +0200

    Added login from master

--- lib/screens/login/sign_in_screen.dart
+++ lib/screens/login/sign_in_screen.dart
@@ -1,43 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:movies/screens/login/sign_up_screen.dart';
+
 class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     void _signInButtonPressed() {}
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        iconTheme: IconThemeData(
-          color: Colors.pink
+          title: Text(
+        'Sign in',
+        style: TextStyle(
+          color: Colors.grey[400],
+          fontSize: 40,
         ),
-      ),
-      backgroundColor: Colors.white,
+      )),
+      backgroundColor: Colors.grey[200],
       body: Container(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 30.0),
-                  child: Text(
-                    'Sign In',
-                    style: TextStyle(
-                      color: Colors.pink,
-                      fontSize: 40,
-                    ),
-                  )),
               SignUpForm(),
-              ButtonTheme(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                child: RaisedButton(
-                  onPressed: () {},
-                  color: Colors.pink,
-                  padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
-                  child: Text('Sign in', style: TextStyle(color: Colors.white, fontSize: 17.0, fontWeight: FontWeight.w300)),
-                ),
-              ),
               Container(
                 margin: EdgeInsets.fromLTRB(0, 14, 0, 0),
                 child: Text(
@@ -66,28 +51,65 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignInFormState extends State<SignUpForm> {
+  var email = "";
+  var password = "";
+  var buttonEnabled = false;
   final _emailTextController = TextEditingController();
   final _passwordTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+
+  bool _emailIsValid() {
+    if (email.contains("@") && email.contains(".com") && email.length > 6) {
+      return true;
+    }
+    return false;
+  }
+
+  void _enableButtonIfPossible() {
+    if (password.length > 4 && _emailIsValid()) {
+      buttonEnabled = true;
+    } else {
+      buttonEnabled = false;
+    }
+    setState(() {});
+  }
+
+  void _singInWithFirebase() async {
+    try {
+      final user = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      if (user != null) {
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      print("whoops something went wrong");
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
+    return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
-            child: TextFormField(
+            child: TextField(
+              onChanged: (email) {
+                this.email = email;
+                _enableButtonIfPossible();
+              },
               controller: _emailTextController,
               decoration: InputDecoration(
                 hintText: 'email',
                 hintStyle: TextStyle(color: Colors.grey[400]),
                 enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink[50]),
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
                 ),
                 focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink),
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
                 ),
               ),
               style: TextStyle(
@@ -96,23 +118,45 @@ class _SignInFormState extends State<SignUpForm> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 30.0),
-            child: TextFormField(
+            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+            child: TextField(
+              onChanged: (password) {
+                this.password = password;
+                _enableButtonIfPossible();
+              },
               obscureText: true,
               controller: _passwordTextController,
               decoration: InputDecoration(
                 hintText: 'password',
                 hintStyle: TextStyle(color: Colors.grey[400]),
                 enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink[50]),
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
                 ),
                 focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink),
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
                 ),
               ),
               style: TextStyle(
                 color: Colors.blueGrey[600],
               ),
+            ),
+          ),
+          FlatButton(
+            padding: EdgeInsets.all(10),
+            splashColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+            onPressed: () {
+              if (buttonEnabled) {
+                _singInWithFirebase();
+              }
+            },
+            child: Container(
+              decoration: new BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: buttonEnabled ? Colors.blue[100] : Colors.blueGrey[100],
+              ),
+              padding: EdgeInsets.all(10),
+              child: Text('Sign in'),
             ),
           ),
         ],
--- lib/screens/login/sign_up_screen.dart
+++ lib/screens/login/sign_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,37 +8,21 @@ class SignUpScreen extends StatelessWidget {
     void _signInButtonPressed() {}
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        iconTheme: IconThemeData(
-          color: Colors.pink,
+        title: Text(
+          'Sign up',
+          style: TextStyle(
+            color: Colors.grey[400],
+            fontSize: 40,
+          ),
         ),
       ),
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.grey[200],
       body: Container(
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(
-                      color: Colors.pink,
-                      fontSize: 40,
-                    ),
-                  )),
               SignUpForm(),
-              ButtonTheme(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-                child: RaisedButton(
-                  onPressed: () {},
-                    color: Colors.pink,
-                  padding: EdgeInsets.fromLTRB(30.0, 15.0, 30.0, 15.0),
-                  child: Text('Sign up', style: TextStyle(color: Colors.white, fontSize: 17.0, fontWeight: FontWeight.w300)),
-                ),
-              ),
             ]),
       ),
     );
@@ -50,36 +35,80 @@ class SignUpForm extends StatefulWidget {
 }
 
 class _SignUpFormState extends State<SignUpForm> {
-  final _firstNameTextController = TextEditingController();
-  final _lastNameTextController = TextEditingController();
   final _usernameTextController = TextEditingController();
+  final _emailTextController = TextEditingController();
+  final _passwordTextController = TextEditingController();
+  final _auth = FirebaseAuth.instance;
+  var buttonEnabled = false;
+  var username = "";
+  var email = "";
+  var password = "";
 
-  var username;
-  var email;
-  var password;
+  bool _emailIsValid() {
+    if (email.contains("@") && email.contains(".com") && email.length > 6) {
+      return true;
+    }
+    return false;
+  }
+
+  bool _usernameIsValid() {
+    if (username.contains("@") || username.contains(".com")) {
+      return false;
+    }
+    if (username.length == 0 || username.contains(" ")) {
+      return false;
+    }
+
+    return true;
+  }
+
+  void _enableButtonIfPossible() {
+    if (_emailIsValid() && password.length > 4 && _usernameIsValid()) {
+      buttonEnabled = true;
+    } else {
+      buttonEnabled = false;
+    }
+    setState(() {});
+  }
+
+  void _signUpWithFirebase() async {
+    try {
+      print(email);
+      print(password);
+      final newUser = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      int count = 0;
+      Navigator.of(context).popUntil((_) => count++ >= 2);
+      print("success");
+    } catch (e) {
+      print("whoops something went wrong");
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
+    return Container(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
-            child: TextFormField(
-              onChanged: (_firstNameTextController) {
-                username = _firstNameTextController.toString();
+            child: TextField(
+              onChanged: (username) {
+                this.username = username;
+                _enableButtonIfPossible();
               },
-              controller: _firstNameTextController,
+              controller: _usernameTextController,
               decoration: InputDecoration(
                 hintText: 'username',
                 hintStyle: TextStyle(color: Colors.grey[400]),
                 enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink[50]),
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
                 ),
                 focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink),
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
                 ),
               ),
               style: TextStyle(
@@ -89,19 +118,20 @@ class _SignUpFormState extends State<SignUpForm> {
           ),
           Padding(
             padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
-            child: TextFormField(
-              onChanged: (_firstNameTextController) {
-                email = _lastNameTextController.toString();
+            child: TextField(
+              onChanged: (email) {
+                this.email = email;
+                _enableButtonIfPossible();
               },
-              controller: _lastNameTextController,
+              controller: _emailTextController,
               decoration: InputDecoration(
                 hintText: 'email',
                 hintStyle: TextStyle(color: Colors.grey[400]),
                 enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink[50]),
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
                 ),
                 focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink),
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
                 ),
               ),
               style: TextStyle(
@@ -110,26 +140,41 @@ class _SignUpFormState extends State<SignUpForm> {
             ),
           ),
           Padding(
-            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 30.0),
-            child: TextFormField(
-              onChanged: (_firstNameTextController) {
-                password = _usernameTextController.toString();
+            padding: EdgeInsets.fromLTRB(24.0, 8.0, 24.0, 8.0),
+            child: TextField(
+              onChanged: (password) {
+                this.password = password;
+                _enableButtonIfPossible();
               },
               obscureText: true,
-              controller: _usernameTextController,
+              controller: _passwordTextController,
               decoration: InputDecoration(
                 hintText: 'password',
                 hintStyle: TextStyle(color: Colors.grey[400]),
                 enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink[50]),
+                  borderSide: BorderSide(color: Colors.blueGrey[200]),
                 ),
                 focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.pink),
+                  borderSide: BorderSide(color: Colors.blueGrey[900]),
                 ),
               ),
               style: TextStyle(
                 color: Colors.blueGrey[600],
               ),
+            ),
+          ),
+          FlatButton(
+            onPressed: () {
+              _signUpWithFirebase();
+            },
+            padding: EdgeInsets.all(10),
+            child: Container(
+              decoration: new BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: buttonEnabled ? Colors.blue[100] : Colors.blueGrey[100],
+              ),
+              padding: EdgeInsets.all(10),
+              child: Text('Sign up'),
             ),
           ),
         ],
